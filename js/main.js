7e6d7dfc7622579f87846894b6a8a036df7ddce0
Xiangdong Zhang <zhangxd6@Xiang.local>
2017-03-20 21:36:20 -0500

    why section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,10 @@ $(function() {
 $(window).resize(function() {
     $("section").height($(window).height() - 100);
 })
+
+setInterval(function() {
+    var winWidth = $(window).width();
+    if (winWidth < 1000) {
+        $("section").height('auto')
+    }
+}, 250);
