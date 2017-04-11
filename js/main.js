5eb62d005daf2f66993d811b5ac6b8585863dd7c
X Zhang <zhangxd6@hotmail.com>
2017-04-11 14:20:04 -0500

    hook api

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 $(function() {
 
     $(window).trigger('resize');
+
+    $('#mc-embedded-subscribe').click(function() {
+        var msg = {};
+        msg.from = $('#mce-EMAIL').val();
+        msg.firstName = $('#mce-FNAME').val();
+        msg.lastName = $('#mce-LNAME').val();
+        msg.message = $('#mce-MMERGE3').val() || '';
+
+        $.ajax({
+            type: "POST",
+            url: 'https://mailgun.azurewebsites.net/api/enobio?code=d6AS5KrOTLoEbvv52t4CYBkAeqCwJCNB/fFVRJD6GD5UAqA9RBaf5A==',
+            data: JSON.stringify(msg),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(data) {
+                alert(data);
+            },
+            failure: function(errMsg) {
+                alert(errMsg);
+            }
+        });
+
+    });
 });
 
 $(window).resize(function() {
